8388258679174b0e2a50f785c2cfcc1eba9b2d13
Raviprakash Ammagodanahally Sundare Gowda <242903@na.nttdata.com>
2026-06-02 21:34:25 +0530

    Added key to env

--- src/main/java/com/example/assistant/controller/PolicyController.java
+++ src/main/java/com/example/assistant/controller/PolicyController.java
@@ -1,13 +1,18 @@
 package com.example.assistant.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bot")
 public class PolicyController {
 
+
+    @Value("${spring.ai.google.gemini.api-key}")
+    private String key;
+
     @GetMapping("/status")
     public String status() {
-        return "Service is running...";
+        return "Service is running..."+ key;
     }
 }
